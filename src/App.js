917e76f8b3724e2033d570ff8e8d9ae392eecb9c
Tanvir Hassan <Tanvirtamim72@gmail.com>
2023-01-15 04:58:00 +0600

    add react-scroll-to-top

--- src/App.js
+++ src/App.js
@@ -5,12 +5,15 @@ import BulkUploadButton from './components/BulkUploadButton/BulkUploadButton';
 import Footer from './components/Footer/Footer';
 import NavBar from './components/NavBar/NavBar';
 import ProductTable from './components/ProductTable/ProductTable';
+import ScrollToTop from "react-scroll-to-top";
+import { HiOutlineChevronDoubleUp } from 'react-icons/hi';
 // import ProductList from './components/ProductList/ProductList';
 
 function App() {
   return (
     <div className="">
       <NavBar />
+      <ScrollToTop className='bg-none hover:bg-none text-black font-extrabold rounded-full ' smooth component={<HiOutlineChevronDoubleUp className='-ml-2'/>} />
       <ProductTable/>
       <BulkUploadButton />
         {/* <ProductList /> */}
